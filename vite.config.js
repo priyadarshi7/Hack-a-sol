3f6cfb0d5ddd225071bd35c7ff9f1825a17287a1
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-06-07 15:46:05 +0530

    vite-config-allow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      allow: [
+        'D:/Visual Studio Files/Hack-a-Sol/hack-a-sol/src/assets',
+        'D:/Visual Studio Files/Hack-a-Sol/hack-a-sol',
+        'D:/Visual Studio Files/hack/Hack-a-sol',  // Add this directory to allow Vite to access the correct path
+      ]
+    }
+  }
 })
