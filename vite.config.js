528b8f217e4ba35a43ba76294e307c2ea5074a7d
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-06-07 17:02:33 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/priyadarshi7/Hack-a-sol.git',
+  base: '/Hack-a-sol/',
   server: {
     fs: {
       allow: [
