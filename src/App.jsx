2feb8d9c62ee81d315622e9c1d2b329e94c07e20
Kushagra <kushagraverma1804@gmail.com>
2024-06-07 18:45:54 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Navbar from './Components/Navbar.jsx'
+import MainText from "./Components/MainText.jsx"
 
 export default function App(){
 
@@ -7,6 +8,7 @@ export default function App(){
     return(
         <>
         <Navbar/>
+        <MainText/>
         </>
     )
 }
--- src/Components/MainText.jsx
+++ src/Components/MainText.jsx
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function MainText(){
-    
+    return (
+        <div className="cs">
+            <div className="cont">
+                <h1>HACK-A-SOL</h1>
+                <img src=".\src\assets\69e341362c338a0185f87d2ac3fd1b75.gif" className="img"></img>
+                <p>Annual Pan-India <span className="txt">Hackathon</span><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by IIIT-Naya Raipur</p>
+            </div>
+        </div>
+    )
 }
