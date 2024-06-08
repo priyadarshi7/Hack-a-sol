89f25e3bc3c9e8f8c0626cad016ff1f56b842add
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-06-09 00:14:05 +0530

    load-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react"
 import Navbar from './Components/Navbar.jsx'
 import MainText from "./Components/MainText.jsx"
+import Loading from "./Components/Loading.jsx";
 
 export default function App(){
+    const [loading,setLoading]=React.useState(true);
+    React.useEffect(()=>{
+        setTimeout(()=>setLoading(false),4000)
+    },[])
 
-
-    return(
-        <div className="main">
-        <Navbar/>
-        <MainText/>
-        </div>
-    )
+    if(loading){
+        return(
+            <Loading />
+        )
+    }
+        return(
+            <div className="main">
+            <Navbar/>
+            <MainText/>
+            </div>
+        )
 }
---
+++ src/Components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Loading(){
+    return(
+        <div className="load-screen">
+            <div className="load-icon"></div>
+        </div>
+    )
+}
