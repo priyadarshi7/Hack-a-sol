70098cfb69fc4a6b830a044376fbd397b4cb2e99
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-06-09 02:05:00 +0530

    video added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from './Components/Navbar.jsx'
 import MainText from "./Components/MainText.jsx"
 import Loading from "./Components/Loading.jsx";
+import BgVideo from "./assets/BG_video.mp4"
 
 export default function App(){
     const [loading,setLoading]=React.useState(true);
@@ -12,6 +13,7 @@ export default function App(){
         return(
             
             <div className="main">
+                <video autoPlay loop muted className="vid"><source src={BgVideo} type="video/mp4" /> </video>
                {loading && <Loading loading={loading}/>}
             {loading===false && <Navbar/>}
             {loading===false && <MainText/>}
