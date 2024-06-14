9b1b64ba76f4d90421754b7ae889fcfcb5975afd
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-06-14 14:17:06 +0530

    minor changes

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,10 +1,9 @@
 import React from "react"
-import Hack_logo from "../assets/Hac-a-sol_logo.png"
 
 export default function Loading(props){
     return(
         <div className={props.loading?"load-screen":""}>
-            <img src={Hack_logo} className={props.loading?"load-icon":""}/>
+            <div className={props.loading?"load-icon":""}></div>
         </div>
     )
 }
