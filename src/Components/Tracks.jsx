29c5629d6a818130b81e804bf99d7ee978368cfe
Kushagra <kushagraverma1804@gmail.com>
2024-06-14 20:10:44 +0530

    animations changed

--- src/Components/Tracks.jsx
+++ src/Components/Tracks.jsx
@@ -11,7 +11,7 @@ export default function Tracks() {
             <div className="tracks_heading" data-aos="zoom-in-down" data-aos-duration="500">
                 <h2>TRACKS</h2>
             </div>
-            <div className="tracks_events" data-aos="flip-up" data-aos-delay="200">
+            <div className="tracks_events" data-aos="zoom-in" data-aos-delay="200">
                 <div className="tracks_box"></div>
                 <div className="tracks_box"></div>
                 <div className="tracks_box"></div>
