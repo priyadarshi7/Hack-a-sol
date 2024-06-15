21835d2493c4bd1e7c33217b9e87bff2fa4234d3
Kushagra <kushagraverma1804@gmail.com>
2024-06-15 14:22:42 +0530

    comming soon gif prob resolve

--- src/Components/MainText.jsx
+++ src/Components/MainText.jsx
@@ -1,18 +1,41 @@
-import React from "react"
-import comingSoon from "../assets/coming-soon-ezgif.com-crop.gif"
+import React, { useState } from "react";
+import comingSoon from "../assets/coming-soon-ezgif.com-crop.gif";
 
-export default function MainText(){
-    return (
-        <div className="container">
-            <div className="text-content">
-                <h1>HACK-A-SOL</h1>
-                <img src={comingSoon}/>
-                <h2>Annual Pan-India <span className="txt">Hackathon</span><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by IIIT-Naya Raipur</h2>
-                <div className="buttons">
-                    {/* <div className="button1" onClick={() => window.open(url, '_main')} ><div className="img_registration"></div><div>PRE-REGISTER NOW</div></div> */}
-                    <div className="button2" onClick={() => window.open('https://discord.gg/gejd3wHT', '_main')}><div className="img-discord"></div><div>JOIN OUR DISCORD</div></div></div><div>
-                </div>
+export default function MainText() {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="text-content">
+        <h1>HACK-A-SOL</h1>
+        <div style={{ display: isImageLoaded ? 'none' : 'block' }}>Loading...</div>
+        <img 
+          src={comingSoon} 
+          id="csn" 
+          onLoad={() => setIsImageLoaded(true)} 
+          onError={() => console.error('Error loading image')} 
+          style={{ display: isImageLoaded ? 'block' : 'none' }}
+        />
+        {isImageLoaded && (
+          <>
+            <h2>
+              Annual Pan-India <span className="txt">Hackathon</span>
+              <br></br>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by IIIT-Naya Raipur
+            </h2>
+            <div className="buttons">
+              {/* <div className="button1" onClick={() => window.open(url, '_main')} ><div className="img_registration"></div><div>PRE-REGISTER NOW</div></div> */}
+              <div 
+                className="button2" 
+                onClick={() => window.open('https://discord.gg/gejd3wHT', '_main')}
+              >
+                <div className="img-discord"></div>
+                <div>JOIN OUR DISCORD</div>
+              </div>
             </div>
-        </div>
-           )
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
