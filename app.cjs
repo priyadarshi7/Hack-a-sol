1fb4f5659fc06cbab4d2925a9e85a1347099e505
Kushagra <kushagraverma1804@gmail.com>
2024-06-18 04:22:05 +0530

    backend 5

--- app.cjs
+++ app.cjs
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port =  3000;
 // const mongoose = require("mongoose");
 
 // async function main() {
@@ -38,3 +38,5 @@ app.post("/register", (req, res) => {
     // res.redirect("http://localhost:5173");
     res.send("Hello");
 });
+
+module.exports = app;
