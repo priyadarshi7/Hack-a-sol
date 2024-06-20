f325b5dc64f7130364489f0b0ad56a0b13693c0e
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-06-20 23:54:20 +0530

    change

--- src/Components/MainText.jsx
+++ src/Components/MainText.jsx
@@ -12,7 +12,7 @@ export default function MainText(props){
                 <img src={comingSoon} alt="" data-aos="zoom-in" data-aos-duration="1000"/>
                 <h2 data-aos-duration="500" data-aos="zoom-in">Annual Pan-India <span className="txt">Hackathon</span><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by IIIT-Naya Raipur</h2>
                 <div className="buttons">
-                    <div className="button1" onClick={props.modalAppear}><div className="img_registration"></div><div>PRE-REGISTER NOW</div></div>
+                    {/* <div className="button1" onClick={props.modalAppear}><div className="img_registration"></div><div>PRE-REGISTER NOW</div></div> */}
                     <div className="button2" onClick={() => window.open('https://discord.gg/gejd3wHT', '_main')}><div className="img-discord"></div><div>JOIN OUR DISCORD</div></div></div><div>
                 </div>
             </div>
