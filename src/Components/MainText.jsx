035277c3fbe5b071ce78ac8dd745dc5566743c86
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-06-23 20:03:23 +0530

    discord link changed

--- src/Components/MainText.jsx
+++ src/Components/MainText.jsx
@@ -13,7 +13,7 @@ export default function MainText(props){
                 <h2 data-aos-duration="500" data-aos="zoom-in">Annual Pan-India <span className="txt">Hackathon</span><br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by IIIT-Naya Raipur</h2>
                 <div className="buttons">
                     <div className="button1" onClick={props.modalAppear}><div className="img_registration"></div><div>PRE-REGISTER NOW</div></div>
-                    <div className="button2" onClick={() => window.open('https://discord.gg/gejd3wHT', '_main')}><div className="img-discord"></div><div>JOIN OUR DISCORD</div></div></div><div>
+                    <div className="button2" onClick={() => window.open('https://discord.gg/H24uuScGG9', '_main')}><div className="img-discord"></div><div>JOIN OUR DISCORD</div></div></div><div>
                 </div>
             </div>
         </div>
