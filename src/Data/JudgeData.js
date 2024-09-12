d23ac9d04eb00d1ec9c81a99020afe29d00d1fa1
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-09-12 22:01:47 +0530

    Judge 4

--- src/Data/JudgeData.js
+++ src/Data/JudgeData.js
@@ -1,6 +1,7 @@
 import Judge1 from "../assets/Judge/Judge1.png"
 import Judge2 from "../assets/Judge/Judge2.jpg"
 import Judge3 from "../assets/Judge/Judge3.jpg"
+import Judge4 from "../assets/Judge/Judge4.png"
 const JudgeData =[
     {
         img: Judge1,
@@ -19,6 +20,12 @@ const JudgeData =[
         name: "Ayush Guha",
         designation: "Cyber Forensic and Law Expert",
         Instagram: "https://www.instagram.com/cyberexpertayushguha?igsh=ZjZjN3l6dHlvbTZk"
+    },
+    {
+        img: Judge4,
+        name: "Lohit Paul",
+        designation: "D.R.D.O Cybersecurity Information Assurance Professional",
+        linkedIn: "https://www.linkedin.com/in/lohit-paul-17440a1b2/"
     },
 ]
 
