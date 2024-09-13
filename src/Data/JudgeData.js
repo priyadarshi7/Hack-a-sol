45e1458621ccdff11aaa7338c7d6e5d831e22e51
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-09-13 13:30:46 +0530

    Judge 5

--- src/Data/JudgeData.js
+++ src/Data/JudgeData.js
@@ -2,6 +2,8 @@ import Judge1 from "../assets/Judge/Judge1.png"
 import Judge2 from "../assets/Judge/Judge2.jpg"
 import Judge3 from "../assets/Judge/Judge3.jpg"
 import Judge4 from "../assets/Judge/Judge4.png"
+import Judge5 from "../assets/Judge/Judge5.jpg"
+
 const JudgeData =[
     {
         img: Judge1,
@@ -26,6 +28,12 @@ const JudgeData =[
         name: "Lohit Paul",
         designation: "D.R.D.O Cybersecurity Information Assurance Professional",
         linkedIn: "https://www.linkedin.com/in/lohit-paul-17440a1b2/"
+    },
+    {
+        img: Judge5,
+        name: "Adarsh Gupta",
+        designation: "Software Development Engineer Infosys",
+        linkedIn: "https://www.linkedin.com/in/adarsh-gupta-90b60a111?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "
     },
 ]
 
